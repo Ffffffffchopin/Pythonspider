ee8b545a03306d1b39eb73b9f9f43c26017fd877
xsy <1131585528@qq.com>
2022-09-25 16:32:32 +0800

    dd

--- goood.py
+++ goood.py
@@ -5,6 +5,12 @@ import sys
 init_url='https://www.gooood.cn/category/type/architecture'
 store_path='gooood'
 
+def isChinese(word):
+    for ch in word:
+        if '\u4e00' <= ch <= '\u9fff':
+            return True
+    return False
+
 class gooodCollectionSpider(SpiderHTML):
     def __init__(self,init_url,pageEnd):
         self.init_url=init_url
@@ -13,14 +19,26 @@ class gooodCollectionSpider(SpiderHTML):
         
     def collectLink(self):
         hrefs=[]
-        for i in tqdm(range(1,923)):
+        for i in tqdm(range(1,self.pageEnd),desc='collectLink'):
             page_url=self.init_url+'/{}'.format(i)
             content=self.getUrl(page_url)
-            href=content.find_all('a',class_='cover-link',href=True).attrs['href']
-            hrefs.append(href)
+            a_list=content.find_all('a',class_='cover-link',href=True)
+            for a in a_list:
+                hrefs.append(a.attrs['href'])
+
+            #hrefs.append(href)
             return hrefs
+    
+    def collectData(self,hrefs):
+        for i in tqdm(range(len(hrefs)),desc="collectData"):
+            url= "https://www.gooood.cn/"+str(hrefs[i])
+            
+
+    
 
 if __name__=='__main__':
     pageEnd=sys.argv[1]
     spider=gooodCollectionSpider(init_url,pageEnd)
-    print(spider.collectLink())
+    hrefs=spider.collectLink()
+    spider.collectData(hrefs)
+    
